bd668cf84eaa0add8b0cb9e0ec57038508a9216f
k2733 <18296651292@163.com>
2022-07-03 01:05:59 +0800

    20220703:01:05

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 from app.my_list import list_bp
+from app.my_list import list_index_bp
 
 app = Flask(__name__)
-app.register_blueprint(list_bp)
+app.register_blueprint(list_bp)  #注册蓝图
+app.register_blueprint(list_index_bp)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('首页.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/my_list.py
+++ app/my_list.py
@@ -1,7 +1,20 @@
 from flask import Blueprint,render_template
 
-list_bp = Blueprint("list",__name__,url_prefix="/list")  #前缀后的路由
+list_bp = Blueprint("list",__name__,url_prefix="/list")  #url_predix指定蓝图路由前缀
+list_index_bp = Blueprint("index",__name__,url_prefix="/index")  
 
-@list_bp.route("/")  #前缀
-def my_list():
+@list_bp.route("/")
+def list():
     return render_template("list.html")
+
+@list_bp.route("/one")  #指定蓝图路由前缀后的路由
+def my_list():
+    return render_template("list_one.html")
+
+@list_index_bp.route("/")
+def list_index():
+    return render_template("index.html")
+
+@list_index_bp.route("/one")
+def my_list_index():
+    return render_template("index_one.html")
